a58c2ebcfc5b75a3e17e6e978ea951e15139a5b8
LvuhaCool <ghabaruk@gmail.com>
2026-06-01 22:57:42 +0300

    make a functional imt calculator

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Hello, World!")
+	const IMTPower = 2
+	var userHeight float64
+	var userWeight float64
+	fmt.Println("___ Калькулятор индекса массы тела ___")
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес в килограммах: ")
+	fmt.Scan(&userWeight)
+	IMT := userWeight / math.Pow(userHeight, IMTPower)
+	fmt.Printf("Ваш индекс массы тела: %v", IMT)
 }
